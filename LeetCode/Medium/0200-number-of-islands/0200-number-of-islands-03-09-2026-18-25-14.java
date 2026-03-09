e6317b8fdd017c4a84ee4c5bc064944eacdeddbb
Yuvraj Kishore Sinha <yuvraj.k.sinha@gmail.com>
2026-03-09 18:25:10 +0530

    Problem : 0200-number-of-islands Difficulty : Medium Topic : Array Time : 3 ms (86.64%) Space :52.5 MB (31.46%) Language used : .java Date :  03-09-2026

---
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands-03-09-2026-18-25-14.java
@@ -0,0 +1,27 @@
+//graph- dfs
+class Solution {
+    public int rows,columns,count=0;
+    public int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+    public int numIslands(char[][] grid) {
+        this.rows = grid.length;
+        this.columns = grid[0].length;
+        for(int r=0;r<rows;r++){
+            for(int c=0;c<columns;c++){
+                if(grid[r][c]=='1'){
+                    dfs(grid,r,c);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public void dfs(char[][] grid,int r,int c){
+        if(r<0||c<0||r>=rows||c>=columns||grid[r][c]=='0'){
+            return;
+        }
+        grid[r][c]='0'; //visited
+        for(int[] d:directions){
+            dfs(grid,r+d[0],c+d[1]);
+        }
+    }
+}
