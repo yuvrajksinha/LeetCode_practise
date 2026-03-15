460977a53f989792d79d290905a1418069337708
Yuvraj Kishore Sinha <yuvraj.k.sinha@gmail.com>
2026-03-15 22:49:39 +0530

    Problem : 0743-network-delay-time Difficulty : Medium Topic : Depth-First Search Time : 13 ms (50.68%) Space :49.3 MB (49.25%) Language used : .java Date :  03-15-2026

---
+++ LeetCode/Medium/0743-network-delay-time/0743-network-delay-time-03-15-2026-22-49-46.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        Map<Integer,List<int[]>> graph = new HashMap<>();
+        for(int[] time:times){
+            int source = time[0];
+            int destination = time[1];
+            int timeToDest = time[2];
+            if(!graph.containsKey(source)){
+                graph.put(source,new ArrayList<>());
+            }
+            graph.get(source).add(new int[]{destination,timeToDest});
+        }
+        int[] dist = new int[n+1];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[k]=0;
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b)->a[0]-b[0]);
+        minHeap.add(new int[]{0,k});
+        while(!minHeap.isEmpty()){
+            int[] currNode = minHeap.poll();
+            int distance = currNode[0];
+            int node = currNode[1];
+            if(distance>dist[node]){
+                continue;
+            }
+            if(!graph.containsKey(node)){
+                continue;
+            }
+            for(int[] neighbour:graph.get(node)){
+                int next = neighbour[0];
+                int weight = neighbour[1];
+                if(distance+weight<dist[next]){
+                    dist[next]=distance+weight;
+                    minHeap.add(new int[]{dist[next],next});
+                }
+            }
+        }
+        int maxTime=0;
+        for(int i=1;i<=n;i++){
+            maxTime = Math.max(maxTime,dist[i]);
+        }
+        return maxTime==Integer.MAX_VALUE?-1:maxTime;
+    }
+}
