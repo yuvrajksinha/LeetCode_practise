afe1df9381d4237dead3bec58ce0307bc5f662c2
Yuvraj Kishore Sinha <yuvraj.k.sinha@gmail.com>
2026-03-22 22:50:13 +0530

    Problem : 0215-kth-largest-element-in-an-array Difficulty : Medium Topic : Array Time : 57 ms (60.94%) Space :69.1 MB (34.11%) Language used : .java Date :  03-22-2026

---
+++ LeetCode/Medium/0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array-03-22-2026-22-50-12.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a,b)->a-b);
+        for(int num:nums){
+            minHeap.add(num);
+            if(minHeap.size()>k){
+                minHeap.poll();
+            }
+        }
+        return minHeap.peek();
+    }
+}
