07095b670bb7f24bced95b2e2d070b88e505ed99
Yuvraj Kishore Sinha <yuvraj.k.sinha@gmail.com>
2026-03-29 21:01:59 +0530

    Problem : 0347-top-k-frequent-elements Difficulty : Medium Topic : Array Time : 16 ms (40.2%) Space :47.8 MB (41.19%) Language used : .java Date :  03-29-2026

---
+++ LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements-03-29-2026-21-02-02.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer,Integer> freq = new HashMap<>();
+        for(int num:nums){
+            freq.put(num,freq.getOrDefault(num,0)+1);
+        }
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a,b)->freq.get(a)-freq.get(b));
+        for(int num:freq.keySet()){
+            minHeap.add(num);
+            if(minHeap.size()>k){
+                minHeap.poll();
+            }
+        }
+        int[] result = new int[k];
+        for(int i=0;i<k;i++){
+            result[i]=minHeap.poll();
+        }
+        return result;
+    }
+}
